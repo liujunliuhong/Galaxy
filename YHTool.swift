5f41c2a3d0dfd643f1eaa38a64d0c4dad37211ba
qinyuelong <qinyuelong.cn@gmail.com>
2019-06-27 20:50:47 +0800

    新增

--- YHTool.swift
+++ YHTool.swift
@@ -19,3 +19,66 @@ func YHOpenIphoneSettings() {
         }
     }
 }
+
+// Open App Store.
+func YHOpenAppStore(with appID: String) {
+    DispatchQueue.main.async {
+        let urlString = "https://itunes.apple.com/app/id\(appID)"
+        let url = URL(string: urlString)!
+        if #available(iOS 10.0, *) {
+            UIApplication.shared.open(url, options: [:], completionHandler: nil)
+        } else {
+            UIApplication.shared.openURL(url)
+        }
+    }
+}
+
+// Open App Store Review.
+func YHOpenAppStoreReview(with appID: String) {
+    DispatchQueue.main.async {
+        let urlString = "https://itunes.apple.com/cn/app/id\(appID)?action=write-review"
+        let url = URL(string: urlString)!
+        if #available(iOS 10.0, *) {
+            UIApplication.shared.open(url, options: [:], completionHandler: nil)
+        } else {
+            UIApplication.shared.openURL(url)
+        }
+    }
+}
+
+// Make call.
+func YHMakeCall(with tel: String) {
+    DispatchQueue.main.async {
+        let urlString = "tel://\(tel)"
+        let url = URL(string: urlString)!
+        if #available(iOS 10.0, *) {
+            UIApplication.shared.open(url, options: [:], completionHandler: nil)
+        } else {
+            UIApplication.shared.openURL(url)
+        }
+    }
+}
+
+
+/// Open Safari
+///
+/// - Parameters:
+///   - url: url
+///   - autoCorrect: Whether to automatically correct the url, such as adding http://
+func YHOpenSafari(with url: String, autoCorrect: Bool) {
+    
+}
+
+// Get local JSON file content.
+func YHGetLocalJSONFile(file fileName: String) -> Data? {
+    let path = Bundle.main.path(forResource: fileName, ofType: "json") ?? ""
+    // 将错误转换为可选值
+    return try? Data(contentsOf: URL(fileURLWithPath: path))
+}
+
+// Get local Plist file content.
+func YHGetLocalPlistFile(file fileName: String) -> Data? {
+    let path = Bundle.main.path(forResource: fileName, ofType: "plist") ?? ""
+    // 将错误转换为可选值
+    return try? Data(contentsOf: URL(fileURLWithPath: path))
+}

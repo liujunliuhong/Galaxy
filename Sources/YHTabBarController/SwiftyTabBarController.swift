4863d30bb264529d2ec2b2c79189bbf0d6821b93
liujun <1035841713@qq.com>
2020-06-15 09:49:03 +0800

    123

---
+++ Sources/YHTabBarController/SwiftyTabBarController.swift
@@ -0,0 +1,29 @@
+//
+//  SwiftyTabBarController.swift
+//  SwiftTool
+//
+//  Created by apple on 2020/6/11.
+//  Copyright © 2020 yinhe. All rights reserved.
+//
+
+import UIKit
+
+open class SwiftyTabBarController: UITabBarController {
+
+    open override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
+
+extension SwiftyTabBarController {
+    
+}
+
+extension SwiftyTabBarController {
+    
+}
+
+extension SwiftyTabBarController {
+    
+}

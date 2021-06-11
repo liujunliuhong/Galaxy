63f6236daa43e893d14f0c43efe9ba31ebc4a86f
liujun <1035841713@qq.com>
2021-06-11 17:51:25 +0800

    12345

---
+++ Sources/BlockChain/BTC/BTCOPCode.swift
@@ -0,0 +1,15 @@
+//
+//  BTCOPCode.swift
+//  Galaxy
+//
+//  Created by liujun on 2021/6/11.
+//
+
+import Foundation
+
+public struct BTCOPCode {
+    
+    public enum OPCode {
+        
+    }
+}

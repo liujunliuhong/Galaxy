a872ceee86fe1c72056454866bfdad98f747ed09
银河 <1035841713@qq.com>
2021-06-17 23:24:21 +0800

    1234

--- Sources/BlockChain/BTC/BTCOPCodeHelper.swift
+++
@@ -1,8 +0,0 @@
-//
-//  BTCOPCodeHelper.swift
-//  Galaxy
-//
-//  Created by liujun on 2021/6/17.
-//
-
-import Foundation
---
+++ Sources/BlockChain/BTC/BTCScriptChunk.swift
@@ -0,0 +1,13 @@
+//
+//  BTCScriptChunk.swift
+//  Galaxy
+//
+//  Created by galaxy on 2021/6/17.
+//
+
+import Foundation
+
+
+public class BTCScriptChunk {
+    
+}
